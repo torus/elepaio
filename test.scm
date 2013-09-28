af8044780a5192b4d8755be1c16544f0ca58f047
Hisai Toru <toru@torus.jp>
2013-09-28 21:52:33 +0900

    wip

--- test.scm
+++ test.scm
@@ -1,4 +1,6 @@
 (use gauche.test)
+(use www.cgi.test)
+(use sxml.serializer)
 (use redis)
 
 (test-start "elepaio")
@@ -10,8 +12,8 @@
 
 (define user-id 12345)
 (define thread-id 987654)
-(define content '((screen-name . "Gwaihir")
-                  (text . "Hi, there!")))
+(define content '((screen-name "Gwaihir")
+                  (text "Hi, there!")))
 (define room (string-append "test-room-" (x->string (sys-time))))
 
 (test-section "low level APIs")
@@ -42,6 +44,8 @@
        (read-from-string
         (vector-ref (redis-lrange *redis* (elepaio-get-room-key room) -1 -1) 0)))
 
+(test-section "read")
+
 (test* "get the first post"
        `((elepaio-entry (room . ,room)
                         (user-id . ,user-id)
@@ -61,5 +65,16 @@
                         (thread-id . ,thread-id)
                         (content . ,content)))
        (elepaio-get-latest-entries *elep* room 10))
+
+
+(test-section "push CGI")
+
+(define post-content #?=(srl:sxml->xml `(content ,@content)))
+
+(run-cgi-script->sxml "push.cgi"
+                      :parameters `((room . ,room)
+                                    (user-id . ,user-id)
+                                    (thread-id . ,thread-id)
+                                    (content . ,post-content)))
 
 (test-end)
