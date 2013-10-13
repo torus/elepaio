d7369482cdd2741420f93ae4d9a57869178ba6e6
Hisai Toru <toru@torus.jp>
2013-10-13 19:22:32 +0900

    refactoring

--- public/elepaio.js
+++ public/elepaio.js
@@ -59,56 +59,118 @@ function match_and_append_message(msg, container) {
     return last_index
 }
 
+function message_form(room, onsubmit) {
+    return E_("div", {class: "row"},
+              E_("div", {class: "col-md-12"},
+                 function(doc) {
+                     var textarea
+                     var e = E_("form", {role: "form"},
+                                E_("div", {class: "form-group"},
+                                   function(doc) {
+                                       var e =
+                                           E_("textarea", {class: "form-control", rows: 3})
+                                       var ele = e(doc)
+                                       textarea = ele
+                                       return ele
+                                   }),
+                                E_("button", {type: "submit", class: "btn btn-primary"},
+                                   "Submit"))
+                     var ele = e(doc)
+
+                     ele.onsubmit = function() {
+                         // if (textarea.value.match(/^\s*$/))
+                         //     return false
+
+                         console.log("submit!", textarea.value)
+                         var text = textarea.value
+                         var screen_name = "とおる。"
+                         textarea.value = ""
+                         var content_xml = E_("xxx", {},
+                                              E_("content", {},
+                                                 E_("screen-name", {}, screen_name),
+                                                 E_("text", {}, text)))(doc).innerHTML
+                         console.log(content_xml)
+                         $.post("push.cgi", {
+                             room: room,
+                             "user-id": 123,
+                             "thread-id": 0,
+                             content: content_xml,
+                         }, console.log.bind(console))
+
+                         onsubmit()
+                         // interval = 500
+                         // clearTimeout(timeout_id)
+                         // timeout_id = setTimeout(reload, interval)
+                         return false
+                     }
+
+                     return ele
+                 }))
+}
+
 $(document).ready(function(){
     var room = "elepaio"
+    var interval = 1000
+    var timeout_id
     var container
     var e = E_("div", {class: "container"},
+               E_("h1", {}, room),
                function(doc) {
                    var e = E_("div", {id: "messages"})(doc)
                    container = e
                    return e
                },
-               E_("div", {class: "row"},
-                  E_("div", {class: "col-md-12"},
-                     function(doc) {
-                         var textarea
-                         var e = E_("form", {role: "form"},
-                                    E_("div", {class: "form-group"},
-                                       function(doc) {
-                                           var e =
-                                               E_("textarea", {class: "form-control", rows: 3})
-                                           var ele = e(doc)
-                                           textarea = ele
-                                           return ele
-                                       }),
-                                    E_("button", {type: "submit", class: "btn btn-primary"},
-                                       "Submit"))
-                         var ele = e(doc)
+               message_form(room, function() {
+                   interval = 500
+                   clearTimeout(timeout_id)
+                   timeout_id = setTimeout(reload, interval)
+               })
+               // E_("div", {class: "row"},
+               //    E_("div", {class: "col-md-12"},
+               //       function(doc) {
+               //           var textarea
+               //           var e = E_("form", {role: "form"},
+               //                      E_("div", {class: "form-group"},
+               //                         function(doc) {
+               //                             var e =
+               //                                 E_("textarea", {class: "form-control", rows: 3})
+               //                             var ele = e(doc)
+               //                             textarea = ele
+               //                             return ele
+               //                         }),
+               //                      E_("button", {type: "submit", class: "btn btn-primary"},
+               //                         "Submit"))
+               //           var ele = e(doc)
 
-                         ele.onsubmit = function() {
-                             // if (textarea.value.match(/^\s*$/))
-                             //     return false
+               //           ele.onsubmit = function() {
+               //               // if (textarea.value.match(/^\s*$/))
+               //               //     return false
 
-                             console.log("submit!", textarea.value)
-                             var text = textarea.value
-                             var screen_name = "とおる。"
-                             textarea.value = ""
-                             var content_xml = E_("xxx", {},
-                                                  E_("content", {},
-                                                     E_("screen-name", {}, screen_name),
-                                                     E_("text", {}, text)))(doc).innerHTML
-                             console.log(content_xml)
-                             $.post("push.cgi", {
-                                     room: room,
-                                     "user-id": 123,
-                                     "thread-id": 0,
-                                     content: content_xml,
-                             }, console.log.bind(console))
-                             return false
-                         }
+               //               console.log("submit!", textarea.value)
+               //               var text = textarea.value
+               //               var screen_name = "とおる。"
+               //               textarea.value = ""
+               //               var content_xml = E_("xxx", {},
+               //                                    E_("content", {},
+               //                                       E_("screen-name", {}, screen_name),
+               //                                       E_("text", {}, text)))(doc).innerHTML
+               //               console.log(content_xml)
+               //               $.post("push.cgi", {
+               //                       room: room,
+               //                       "user-id": 123,
+               //                       "thread-id": 0,
+               //                       content: content_xml,
+               //               }, console.log.bind(console))
 
-                         return ele
-                     })))
+               //               interval = 500
+               //               clearTimeout(timeout_id)
+               //               timeout_id = setTimeout(reload, interval)
+               //               return false
+               //           }
+
+               //           return ele
+               //       }))
+              )
 
     $(document.body).append(e(document))
 
@@ -118,17 +180,25 @@ $(document).ready(function(){
               last_index = match_and_append_message(msg, container)
           })
 
-    var int_id = setInterval(function() {
+    var reload = function() {
         $.get("pull.cgi", {room: room, after: last_index},
               function(msg) {
-                  last_index = Math.max(last_index,
+                  var idx = Math.max(last_index,
                                         match_and_append_message(msg, container))
+                  if (idx > last_index) {
+                      last_index = idx
+                      interval = 500
+                  } else {
+                      // double the interval (up to 1 minite) if no message received
+                      interval = Math.min(interval * 2, 60 * 1000)
+                  }
+                  setTimeout(reload, interval)
               })
             .fail(function() {
                 console.log("Failed")
-                clearInterval(int_id)
             })
-    }, 1000)
+    }
+    timeout_id = setTimeout(reload, interval)
     // $(container).append(make_message("やまや", "やまやまや")(document))
     // $(container).append(make_message("まや", "やまや")(document))
 })
