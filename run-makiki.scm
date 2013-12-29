f85ce663527cf711fb3edd0d21b4032e0faa8abd
Hisai Toru <toru@torus.jp>
2013-12-30 01:22:09 +0900

    clean up

--- run-makiki.scm
+++ run-makiki.scm
@@ -2,20 +2,6 @@
 
 (add-load-path "./lib" :relative)
 
-#;(use elepaio.handler.pusher-key)
-
-#;(define-http-handler #/^(\/1)?\/pusher-key(.js)?(.cgi)?/
-  (lambda (req app)
-    (slot-set! req 'params (acons "b" 789 (slot-ref req 'params)))
-    ((cgi-script "./pusher-key.cgi") req app)))
-
-
-#;(define-http-handler #/^(\/1)?\/pusher-key(.js)?(.cgi)?/ (cgi-script "./pusher-key.cgi"))
-#;(define-http-handler #/^\/1\/register$/ (cgi-script "./register.cgi"))
-
-;; (define-http-handler #/\/1\/pull$/ (cgi-script "./pull.cgi"))
-;; (define-http-handler #/\/1\/push$/ (cgi-script "./push.cgi"))
-
 ;; /1/resource/method[.ext]
 
 (define-http-handler #/\/1\/([\w-]+)\/([\w-]+)(\.\w+)?\/?$/
